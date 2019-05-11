e6db41b70b39cabf69e81d34ddbdc56440c14396
Blue <1052446882@qq.com>
2019-05-11 11:26:26 +0800

    修改配置文件

--- application/config.php
+++ application/config.php
@@ -140,7 +140,7 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__PUBLIC__'=>'/pdd_two/public',
+        '__PUBLIC__'=>'/pdd/public',
         '__ROOT__' => '/',
         '__APP__' => '/index.php',
     ],
